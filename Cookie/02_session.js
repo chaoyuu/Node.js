14e0e8503900068e139458a7015a34fab82a19a6
chaoyuu <chaoyu995@gmail.com>
2023-07-28 00:13:02 +0800

    更新cookie

---
+++ Cookie/02_session.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+
+const app = express();
+
+
+
+app.get('/',(req,res)=>{
+  res.send('home')
+
+});
+
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+});
