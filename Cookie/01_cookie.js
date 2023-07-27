ccf58bd1648ecd5210dbc4b7248c488aa51c5fef
chaoyuu <chaoyu995@gmail.com>
2023-07-28 00:10:00 +0800

    更新获取cookie

--- Cookie/01_cookie.js
+++ Cookie/01_cookie.js
@@ -1,7 +1,8 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
 
 const app = express();
-
+app.use(cookieParser());
 app.get('/set-cookie',(req,res)=>{
     // res.cookie('name','yuyu');
     res.cookie('name','lisi',{maxAge:60 *1000});
@@ -15,6 +16,12 @@ app.get('/remove-cookie',(req,res)=>{
  res.send('删除成功了哦')
 
 });
+
+//获取cookie
+app.get('/get-cookie',(req,res)=>{
+    console.log(req.cookies);
+    res.send(`我来了哦 ${req.cookies.name}`);
+})
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
 });
